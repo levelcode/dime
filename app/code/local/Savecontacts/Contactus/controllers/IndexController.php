8551b0acfc1ddd0a96005ad2a1b5ee6193099a23
Camilo Vanegas <kmilo.vanegas@gmail.com>
2018-03-21 14:11:48 -0500

    Adecuación para metdato ciudad

--- app/code/local/Savecontacts/Contactus/controllers/IndexController.php
+++ app/code/local/Savecontacts/Contactus/controllers/IndexController.php
@@ -8,11 +8,62 @@ class Savecontacts_Contactus_IndexController extends Mage_Contacts_IndexControll
      
 
      /* code to save query , you can write our insert query here*/
-     if ( $post ) {
-        
-        $model = Mage::getModel('contactus/contactus');	
-  			$model->setData($post);
-  			$model->save();
+     if ($post) {
+
+      if (!Zend_Validate::is(trim($post['name']) , 'NotEmpty')) {
+        $error = true;
+      }
+
+      if (!Zend_Validate::is(trim($post['comment']) , 'NotEmpty')) {
+        $error = true;
+      }
+
+      if (!Zend_Validate::is(trim($post['email']), 'EmailAddress')) {
+        $error = true;
+      }
+
+      if (Zend_Validate::is(trim($post['hideit']), 'NotEmpty')) {
+        $error = true;
+      }
+
+      if ($error) {
+        throw new Exception();
+      }
+
+      if(isset($post['telephone'])){
+        $tele = trim($post['telephone']);
+      } else {
+        $tele = '';
+      }
+
+      if(isset($post['empresa'])){
+        $empresa = trim($post['empresa']);
+      } else {
+        $empresa = '';
+      }
+
+      if(isset($post['ciudad'])){
+        $ciudad = trim($post['ciudad']);
+      } else {
+        $ciudad = '';
+      }
+
+      if(isset($post['como'])){
+        $como = trim($post['como']);
+      } else {
+        $como = '';
+      }
+
+
+      $model = Mage::getModel('contacts/contacts');
+      $model->setGuestName(trim($post['name']));
+      $model->setGuestEmail(trim($post['email']));
+      $model->setGuestTelephone($tele);
+      $model->setGuestCiudad($ciudad);
+      $model->setGuestComments(trim($post['comment']));
+
+      $model->save();
+
     }
   }
 }
--- app/code/local/Savecontacts/Contactus/sql/contactus_setup/mysql4-install-0.1.0.php
+++ app/code/local/Savecontacts/Contactus/sql/contactus_setup/mysql4-install-0.1.0.php
@@ -11,6 +11,9 @@ CREATE TABLE {$this->getTable('contactus')} (
   `contactus_id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(255) NOT NULL default '',
   `email` varchar(255) NOT NULL default '',
+  `empresa` varchar(255) NOT NULL default '',
+  `ciudad` varchar(255) NOT NULL default '',
+  `como` varchar(255) NOT NULL default '',
   `telephone` varchar(255) NOT NULL default '',
   `comment` text NOT NULL default '',
   `status` smallint(6) NOT NULL default '0',
@@ -18,8 +21,7 @@ CREATE TABLE {$this->getTable('contactus')} (
   `update_time` datetime NULL,
   PRIMARY KEY (`contactus_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- 
-    ");
+ ");
  
 $installer->endSetup(); 
  
